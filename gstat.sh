5b920571a8fcfdf922b2b6c22f23f636a5162dce
root <root@ipa.frk.com>
2019-10-31 19:15:26 +0330

    update

--- gstat.sh
+++ gstat.sh
@@ -3,26 +3,33 @@
 # Script Name: gstat.sh
 #
 # Author: Mahdi khosravi
-# Mail:khosravi.mahdi73@gmail.com
 # Date: 2019/10/30
 #
 # Error Log: Any errors or output associated with the script can be found in /path/to/logfile
 
 ### Online Volume param cheak
-stat=`gluster volume status $1 detail | awk '{print $3}' |grep Y` 
-if [ "$stat" != "Y" ] ;
-        then
-        echo $1 is not Online
-	else
-	echo $1 is Online
-        fi
+#stat=`gluster volume status $@ detail | awk '{print $3}' |grep Y` 
+#for arg in "$@"
+#	do
+#stat=`gluster volume status $arg detail | awk '{print $3}' |grep Y` 
+#	  if [ "$stat" != "Y" ] ;
+#           then
+#      	echo $arg is not Online
+#	     else
+#		echo $arg is Online
+#        fi
+#done
 ### Host status Cheak        
-for peer in $(gluster peer status | grep '^Hostname: ' | awk '{print $2}'); do
-  state=$(gluster peer status | grep -A 2 "$2  $peer$" | grep '^State: ' | sed -nre 's/.* \(([[:graph:]]+)\)$/\1/p')
-  if [ "$state" != "Connected" ]; then
-    exit 0
-  fi
+for i in "$@"
+	do
+stat=`gluster pool list | grep $i | awk '{print $3}' ` 
+	  if [ "$stat" == "Connected" ] ;
+             then
+        	echo $i is Connect
+	     else
+		echo $i is Dis
+          fi
 done
 
 exec 1>>/var/log/gstatoutput.log
-exec 2>>/var/log/gstaterror.log
+exec 2>>/var/log/gstaterror.log##
